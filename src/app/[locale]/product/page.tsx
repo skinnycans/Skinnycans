1d7044b294be1d3b7ef945a5bf9b16c1ffe3c71c
itseghosime-codebase <info.itseghosime@gmail.com>
2025-07-12 11:32:38 -0700

    pages

--- src/app/[locale]/product/page.tsx
+++ src/app/[locale]/product/page.tsx
@@ -1,7 +1,7 @@
 import Collections from '@/components/Collections'
 import React from 'react'
 
-export default function page() {
+export default function ProductPage() {
   return (
     <main className="mx-auto mt-20 w-screen overflow-hidden lg:mt-40">
       <Collections />
