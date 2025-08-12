4611a24705fb1ecd5234fa98c0393ecfc3206c6a
skinnycans <mail@skinnycans.com>
2025-08-11 22:28:15 -0700

    Add files via upload

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import { notFound } from 'next/navigation'
 import { getRequestConfig } from 'next-intl/server'
 
 // Can be imported from a shared config
-export const locales = ['en', 'no', 'se']
+export const locales = ['en']
 export const languageNames: Record<Locale, string> = {
   en: 'English',
   // no: "Norwegian",
